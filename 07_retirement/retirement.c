2f95167159fab5ec849df50944850b0a46a8b177
Coursera Learner <nobody@nowhere.nul>
2020-07-22 21:32:39 +0000

    Did assignment 7

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -1,43 +1,51 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 struct _retire_info{
-  int months;
-  double contribution;
-  double rate_of_return;
+  int months; // number of months
+  double contribution; // amount contributed/spent from the account/month
+  double rate_of_return; // rate of returns
 };
 typedef struct _retire_info retire_info;
 
 
-void retirement(int startAge,
-		double initial,
-		retire_info working,
-		retire_info retired)
-{
-  float balance = initial;
+void retirement(int startAge, double initial,
+		retire_info working, retire_info retired) {
+  /*
+   * This function computes the retirement account balance each
+   * month while you are working and after retirement.
+   *
+   * start_age -> in months
+   * double_initial -> initial savings in dollars
+   */
+
+  double balance = initial;
   for (int i = 0; i < working.months; i++)
     {
-      printf("Age %3d month %2d you have $%.21f\n",startAge/12,startAge%12,balance);
+      printf("Age %3d month %2d you have $%.2f\n",
+	     startAge/12, startAge%12, balance);
       balance += (balance * working.rate_of_return) + working.contribution;
       startAge++;
     }
 
   for (int i = 0; i < retired.months; i++)
     {
-      printf("Age %3d month %2d you have $%.21f\n",startAge/12,startAge%12,balance);
-      balance += (balance * (retired.rate_of_return)) + retired.contribution;
+      printf("Age %3d month %2d you have $%.2f\n",
+	     startAge/12, startAge%12,balance);
+      balance += (balance * retired.rate_of_return) + retired.contribution;
       startAge++;
     }
 }
 
-int main(){
-  retire_info working = {.months = 489, .contribution = 1000.0, .rate_of_return = 0.045/12};
-  retire_info retired = {.months = 384, .contribution = -4000.0, .rate_of_return = 0.01/12};
+int main(void){
+  retire_info working = {.months = 489, .contribution = 1000, .rate_of_return = 0.045/12};
+  retire_info retired = {.months = 384, .contribution = -4000, .rate_of_return = 0.01/12};
 
   int initial_age = 327;
-  double initial_savings = 21345.0;
+  double initial_savings = 21345;
 
-  retirement(initial_age,initial_savings,working,retired);
+  retirement(initial_age, initial_savings, working, retired);
 
   return EXIT_SUCCESS;
 }
+
