9ba3c91092a288926d2cfbd3b7efa138a5496aa4
shivamkukreja16 <68401486+shivamkukreja16@users.noreply.github.com>
2020-07-25 02:21:54 +0530

    Update numToBits.c

--- 19_bits_arr/numToBits.c
+++ 19_bits_arr/numToBits.c
@@ -36,7 +36,24 @@ int getNthBit(uint32_t number, int bit) {
  *   be bit 30 of nums[0], and so on.
  */
 void numToBits(uint32_t * nums, int nNums, int * bits, int nBits) {
+ /*
+   * Input: nums[nNums], bits[nBits]
+   * 
+   */
+  if (nBits < (32*nNums)) {
+    printf("Invalid call to numToBits! nBits is %d, nNums is %d\n", nBits, nNums);
+  }
+  else {
+    int i, j;
+    int count=0;
 
+    for(i=0; i<nNums; i++){
+      for(j=0; j<32; j++){
+	bits[count + j] = getNthBit(nums[i],(31 - j));
+      }
+      count += 32;
+    }
+  }
 }
 
 void doTest(uint32_t * nums, int n) {
