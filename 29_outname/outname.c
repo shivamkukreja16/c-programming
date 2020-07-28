fe9da4865e6c91061e5b30f8ce9cd88482132667
shivamkukreja16 <68401486+shivamkukreja16@users.noreply.github.com>
2020-07-28 23:43:24 +0530

    Update outname.c

--- 29_outname/outname.c
+++ 29_outname/outname.c
@@ -4,5 +4,9 @@
 #include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
-  //WRITE ME
+  char * outFileName = malloc((strlen(inputName)+8)*sizeof(*outFileName));
+  strcpy(outFileName, inputName);
+  strcat(outFileName, ".counts");
+
+  return outFileName;
 }
